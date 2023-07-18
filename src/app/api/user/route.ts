1666407599c01f1b09e76c8af93785d29ad40a98
Frank Villachica <furapay@gmail.com>
2023-07-18 15:30:05 -0500

    FIX (🔨) main: Corrigiendo api users

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -25,7 +25,7 @@ export async function GET(request: Request) {
     const users = await userService.getUsers()
 
     const user = users[0]
-    console.log(format(user.userPlan[0]?.expired_date, 'dd/MM/yyyy'))
+    //console.log(format(user.userPlan[0]?.expired_date, 'dd/MM/yyyy'))
 
     return NextResponse.json(users, { status: 200 })
   } catch (error: any) {
